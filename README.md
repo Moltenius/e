d08b08d163902c4f3a4eb48095004e7851cfd97e
Moltenius <148919902+Moltenius@users.noreply.github.com>
2025-05-18 22:16:15 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# oe
+
+coregui executor for script builders, with some text spoofing to prevent leaks by screenshot
+
+![image](https://user-images.githubusercontent.com/67209229/228671547-de0b5700-6c6e-48cf-aaaf-60e86a97a830.png)
+
+this will work as long as you have serverside access
+
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ceat-ceat/onimaiexecutor/main/client/main.lua", true))()
+```
+
+run the loader above and run the require that is given to you in the script builder's builtin script executor and wait until it connects
+
+you can add an autorun by editing `onimai_autoexec.lua` that is automatically created in your workspace folder but it will only start running the next time you run the executor
+
+supports `krnl` `synapse`
+
+any exploits not above i have no tested with and there may be issues, please make a issue or a pull request if you run into any problems or dm me on discord to alert me immediately `ceat#6144` please tell me that you are making a bug report when dming me or i will ignore you kthx
+
+[[client assets]](https://www.roblox.com/library/12910385605/)
+[[server module]](https://www.roblox.com/library/12910374025/)
---
+++ locations.lua
@@ -0,0 +1,32 @@
+local BASE_URL = "https://raw.githubusercontent.com/ceat-ceat/onimaiexecutor/main"
+local CLIENT_BASE = BASE_URL .. "/client"
+
+return {
+    Main = CLIENT_BASE .. "/main.lua",
+    Modules = {
+        -- antideathedremote
+        AntideathedRemote = CLIENT_BASE .. "/AntideathedRemote/main.lua",
+        BindableEvent = CLIENT_BASE .. "/AntideathedRemote/BindableEvent.lua",
+
+        -- highlighter
+        Highligher = CLIENT_BASE .. "/Highlighter/main.lua",
+        lexer = CLIENT_BASE .. "/Highlighter/lexer.lua",
+        language = CLIENT_BASE .. "/Highlighter/language.lua",
+        
+        -- encryptednet
+        EncryptedNetClient = CLIENT_BASE .. "/EncryptedNetClient/main.lua",
+        EllipticCurveCryptography = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/main.lua",
+        arith = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/arith.lua",
+        chacha20 = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/chacha20.lua",
+        curve = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/curve.lua",
+        modp = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/modp.lua",
+        modq = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/modq.lua",
+        random = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/random.lua",
+        sha256 = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/sha256.lua",
+        twoPower = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/twoPower.lua",
+        util = CLIENT_BASE .. "/EncryptedNetClient/EllipticCurveCryptography/modules/util.lua",
+
+    },
+    ClientAssets = "https://www.roblox.com/library/12910385605/",
+    ServerModule = "https://www.roblox.com/library/12910374025/"
+}
